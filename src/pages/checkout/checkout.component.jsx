8956472a335e2a82f79e4b8549dcf4b5b2c7c1a3
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2021-01-01 23:31:46 +0000

    green

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -32,7 +32,10 @@ const CheckoutPage = ({ cartItems, total }) => (
       </div>
     </div>
     {cartItems.map(cartItem => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      <CheckoutItem
+        key={cartItem.id}
+        cartItem={cartItem}
+      />
     ))}
     <div className='total'>TOTAL: ${total}</div>
     <div className='test-warning'>
